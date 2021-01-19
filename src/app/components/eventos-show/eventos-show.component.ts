2690ed2edf378de4d8c6d83465a25f21cfc6d926
XojeHome <dawxoje@gmail.com>
2021-01-19 08:07:42 +0100

    Componentes juntados,arreglando pipe

--- src/app/components/eventos-show/eventos-show.component.ts
+++ src/app/components/eventos-show/eventos-show.component.ts
@@ -12,6 +12,9 @@ import { IEvento } from 'src/app/Models/IEvento';
 export class EventosShowComponent implements OnInit {
   eventos: IEvento[];
   //@ViewChild("app-eventos-searcher") searcher: EventosSearcherComponent;
+
+  nuevoEvento: IEvento;
+
   sortMode = "";
   textoBusqueda = "";
   constructor() {
@@ -33,6 +36,15 @@ export class EventosShowComponent implements OnInit {
     ];
     //this.sortMode = this.searcher.sortMode;
     //this.textoBusqueda = this.searcher.textoBusqueda;
+
+    /*Form */
+    this.nuevoEvento = {
+      title: "",
+      image: "",
+      date: "",
+      description: "",
+      price: 0
+    };
   }
 
 
@@ -48,6 +60,29 @@ export class EventosShowComponent implements OnInit {
     this.textoBusqueda = "";
     this.sortMode = "price";
     e.preventDefault();
+  }
+  /*Form */
+  addEvento() {
+    this.eventos.push(this.nuevoEvento);
+    console.log(this.nuevoEvento);
+
+    this.nuevoEvento = {
+      title: "",
+      image: "",
+      date: "",
+      description: "",
+      price: 0
+    };
+  }
+  changeImage(e: Event) {
+    const fileInput: HTMLInputElement = e.target as HTMLInputElement;
+    if (!fileInput.files || fileInput.files.length === 0) { return; }
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.addEventListener('loadend', e => {
+      this.nuevoEvento.image = reader.result as string;
+    });
+
   }
 
   ngOnInit(): void {
